303fbb6219ca443e19268c119e63d522545b8848
Jack Munhall <jman777jman@gmail.com>
2022-10-28 16:03:56 -0400

    very bare bones home page

--- simple-crud/client/src/components/Home.js
+++ simple-crud/client/src/components/Home.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import logo from '../assets/Spankys.png'
+import Button from '@mui/material/Button';
+import './Home.css'
+import Typography from '@mui/material/Typography';
   
 function Home(){
-    return <h1>Home page</h1>
+    return (
+        <div class="home">
+            <img src={logo} alt="spanky's logo" class="logo"></img>
+            <Typography variant="h4">I am a ...</Typography>
+            <div class="choices">
+                <Button>Customer</Button>
+                <Button>Employee</Button>
+            </div>
+        </div>
+    );
 }
   
 export default Home;
