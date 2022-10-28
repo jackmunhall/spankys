b5f2471963d2c8819d16139113119741446ba305
Jack Munhall <jman777jman@gmail.com>
2022-10-28 17:29:52 -0400

    home page barebones

--- simple-crud/client/src/components/Home.js
+++ simple-crud/client/src/components/Home.js
@@ -8,10 +8,10 @@ function Home(){
     return (
         <div class="home">
             <img src={logo} alt="spanky's logo" class="logo"></img>
-            <Typography variant="h4">I am a ...</Typography>
+            {/* <Typography variant="h4">I am a ...</Typography> */}
             <div class="choices">
-                <Button>Customer</Button>
-                <Button>Employee</Button>
+                <Button sx={{backgroundColor: 'darkorange'}} variant="contained" >I am a customer</Button>
+                <Button sx={{backgroundColor: 'darkorange'}} variant="contained">I am an employee</Button>
             </div>
         </div>
     );
