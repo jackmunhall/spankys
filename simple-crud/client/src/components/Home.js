010485a09b4ed7cd4da5feb38ad59a4b38f046a8
Ava DeCroix <93365328+a-decroix@users.noreply.github.com>
2022-10-29 14:24:21 -0400

    Home page buttons work, almost done with order form

--- simple-crud/client/src/components/Home.js
+++ simple-crud/client/src/components/Home.js
@@ -3,15 +3,29 @@ import logo from '../assets/Spankys.png'
 import Button from '@mui/material/Button';
 import './Home.css'
 import Typography from '@mui/material/Typography';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+
   
 function Home(){
+
+	const navigate = useNavigate();
+
+	const navigateToCustomer = () => {
+		 navigate('/Customer');
+	}
+
+	const navigateEmployee =() => {
+		 navigate('/employee');
+	}
+
+
     return (
         <div class="home">
             <img src={logo} alt="spanky's logo" class="logo"></img>
             <Typography variant="h4">I am a ...</Typography>
             <div class="choices">
-                <Button>Customer</Button>
-                <Button>Employee</Button>
+                <Button onClick={navigateToCustomer}>Customer</Button>
+                <Button onClick={navigateEmployee}>Employee</Button>
             </div>
         </div>
     );
