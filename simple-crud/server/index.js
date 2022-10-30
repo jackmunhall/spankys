a588ef66bd0afb9ea7dd86b516ff9b656f49d931
jbenite2 <72776470+jbenite2@users.noreply.github.com>
2022-10-30 16:00:05 -0400

    small syntax change

--- simple-crud/server/index.js
+++ simple-crud/server/index.js
@@ -1,5 +1,6 @@
 const express = require("express"); //Instance of the express library
 const app = express(); //Creating an app from the instance
+
 const mysql = require("mysql2"); //Setting up MySQL. Note that we need mysql2
 const cors = require("cors"); //Cross-origin resource sharing
 require("dotenv").config(); //.env file in this folder holds private information
@@ -15,7 +16,6 @@ app.post("/create", (req, res) => {
   const item = req.body.item;
   const qty = parseInt(req.body.qty);
 
-
   db.query(
     "INSERT INTO orders22 (nickname, item, qty) VALUES(?,?,?)",
     [nickname, item, qty],
@@ -29,72 +29,6 @@ app.post("/create", (req, res) => {
   );
 });
 
-
-
-app.get("/orders", (req, res) => {
-	 db.query("SELECT * FROM orders22", (err, result) => {
-		  if (err) {
-				console.log(err);
-		  } else {
-				res.send(result);
-		  }
-
-		 });
-	});
-
-
-app.put("/update", (req, res) => {
-	 const id = req.body.id;
-	 const item = req.body.item;
-
-	 db.query(
-	 	"UPDATE orders22 SET item = ? WHERE id = ?",
-
-		[item, id],
-
-		(err, result) => {
-			 if (err) {
-				  console.log(err);
-			 } else{
-				  res.send(result);
-			 }
-
-			}
-		);
-	});
-
-
-app.put("/updateq", (req, res) => {
-	 const id = req.body.id;
-	 const qty = parseInt(req.body.qty);
-
-	 db.query(
-	 	"UPDATE orders22 SET qty = ? WHERE id = ?",
-		[qty, id],
-
-		(err, result) => {
-			 if(err) {
-				  consle.log(err);
-			  } else {
-					res.send(result);
-			  }
-
-			}
-		);
-});
-
-
-	app.delete("/delete/:id", (req, res) => {
-		 const id = req.params.id;
-		 db.query("DELETE FROM orders22 WHERE id = ?", id, (err, result) => {
-			  if(err) {
-					console.log(err);
-				} else {
-					 res.send(result);
-				}
-			});
-	});
-
-app.listen(3001, () => {
-  console.log("Yay, your server is running in port 3001");
+app.listen(3000, () => {
+  console.log("Yay, your server is running in port 3000");
 });
